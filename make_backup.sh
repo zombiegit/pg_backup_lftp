ca08b95e5f3fb910ad0a7406f371f31831536e46
Sergey Nikulin <s.nikulin@pr-solution.ru>
2017-12-13 15:37:54 +0300

    nerf remove deleted on prod images on backup server

--- make_backup.sh
+++ make_backup.sh
@@ -14,6 +14,6 @@ fi
 DUMP_FILENAME=dump_$DB_NAME_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 pg_dump --dbname=postgresql://$DB_USER:$DB_PASS@postgres:5432/$DB_NAME > /tmp/$DUMP_FILENAME
 
-lftp -c "set ssl:ca-file '/etc/ssl/certs/ca_certs.crt'; set ssl:check-hostname no; ftp:ssl-protect-data true; set xfer:log-file '$LOGS_PATH/lftp.log'; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mkdir -p -f $PROJECT_NAME/sqls; put -O $PROJECT_NAME/sqls /tmp/$DUMP_FILENAME; mirror -c -e -R $UPLOADED_FILES_PATH $PROJECT_NAME/files; bye"
+lftp -c "set ssl:ca-file '/etc/ssl/certs/ca_certs.crt'; set ssl:check-hostname no; ftp:ssl-protect-data true; set xfer:log-file '$LOGS_PATH/lftp.log'; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mkdir -p -f $PROJECT_NAME/sqls; put -O $PROJECT_NAME/sqls /tmp/$DUMP_FILENAME; mirror -c -R $UPLOADED_FILES_PATH $PROJECT_NAME/files; bye"
 
 rm /tmp/$DUMP_FILENAME
