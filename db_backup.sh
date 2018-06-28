1538997b45cafa005616db73658b84b328e4c137
Artem Kanev <a-kanev@yandex.ru>
2018-06-28 15:27:37 +0300

    replace lftp by ssh and rsync

--- db_backup.sh
+++ db_backup.sh
@@ -2,7 +2,7 @@
 
 source /var/www/db_backup.config
 
-if [ -z "$FTP_USER" ] || [ -z "$FTP_PASS" ] || [ -z "$FTP_HOST" ] || [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$UPLOADED_FILES_PATH" ] || [ -z "$LOGS_PATH" ]; then
+if [ -z "$CONNECT_STRING" ] || [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$UPLOADED_FILES_PATH" ] || [ -z "$LOGS_PATH" ]; then
     echo "Insufficient parameters";
     exit 0;
 fi
@@ -27,25 +27,29 @@ make_backup()
     DUMP_FILENAME=dump_$DB_NAME_`date +%d-%m-%Y"_"%H_%M_%S`.sql
     pg_dump --dbname=postgresql://$DB_USER:$DB_PASS@postgres:5432/$DB_NAME > /tmp/$DUMP_FILENAME
 
-    lftp -c "set ssl:ca-file '/etc/ssl/certs/ca_certs.crt'; set ssl:check-hostname no; ftp:ssl-protect-data true; set xfer:log-file '$LOGS_PATH/lftp.log'; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mkdir -p -f $PROJECT_NAME/sqls; put -O $PROJECT_NAME/sqls /tmp/$DUMP_FILENAME; mirror -c -R $UPLOADED_FILES_PATH $PROJECT_NAME/files; bye"
+    echo "mkdir $PROJECT_NAME" | sftp -P 23 $CONNECT_STRING
+    echo "mkdir $PROJECT_NAME/sqls" | sftp -P 23 $CONNECT_STRING
+    echo "put /tmp/$DUMP_FILENAME $PROJECT_NAME/sqls" | sftp -P 23 $CONNECT_STRING
+    rsync -avz -e 'ssh -p23' --progress --ignore-existing $UPLOADED_FILES_PATH $CONNECT_STRING:$PROJECT_NAME/files
 
     rm /tmp/$DUMP_FILENAME
 }
 
 synchronize()
 {
-    LAST_SQL_BACKUP=$(lftp -c "set ssl:ca-file '/etc/ssl/certs/ca_certs.crt'; set ssl:check-hostname no; set xfer:log-file '$LOGS_PATH/lftp.log'; open -u $FTP_USER,$FTP_PASS $FTP_HOST; cd $PROJECT_NAME/sqls; ls -lat | head -2 | tail -1; bye" | awk '{print $9}' | grep '.sql')
+    LAST_SQL_BACKUP=$(echo "ls -lat dfm/sqls" | sftp -P 23 u130584@u130584.your-storagebox.de | sort | tail -4 | head -1 |  awk '{print $9}' | grep '.sql')
 
     if [ -z "$LAST_SQL_BACKUP" ]; then
         echo "Error get sql file path";
         exit 0;
     fi
 
-    echo -n "Static files will be downloaded from backup server. Existing tables from db '$DB_NAME' will be removed and replaced by schema and data from dump file '$LAST_SQL_BACKUP'. Are you sure? (type 'yes' to continue) > ";
+    echo -n "Static files will be downloded from backup server. Existing tables from db '$DB_NAME' will be removed and replaced by schema and data from dump file '$LAST_SQL_BACKUP'. Are you sure? (type 'yes' to continue) > ";
     read answer;
     if [ "$answer" == 'yes' ]; then
 
-        lftp -c "set ssl:ca-file '/etc/ssl/certs/ca_certs.crt'; set ssl:check-hostname no; set xfer:log-file '$LOGS_PATH/lftp.log'; open -u $FTP_USER,$FTP_PASS $FTP_HOST; mget -O '/tmp' $PROJECT_NAME/sqls/$LAST_SQL_BACKUP; bye"
+        echo "get $PROJECT_NAME/sqls/$LAST_SQL_BACKUP /tmp/" | sftp -P 23 $CONNECT_STRING
+        rsync -avz -e 'ssh -p23' --progress --ignore-existing $CONNECT_STRING:$PROJECT_NAME/files $UPLOADED_FILES_PATH
 
         psql -h postgres -p 5432 -U $DB_USER $DB_NAME -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public;"
 
